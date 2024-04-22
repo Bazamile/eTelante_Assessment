563b6fd4b3a6b7158c8b30556e1d8b7a469a0851
BazamileMjoli <bazamilemjoli6@gmail.com>
2024-04-22 19:16:58 +0200

    Instruction

--- src/test/java/Pages/WebTables.java
+++ src/test/java/Pages/WebTables.java
@@ -1,7 +1,4 @@
 package Pages;
-
-import org.checkerframework.checker.units.qual.Force;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
